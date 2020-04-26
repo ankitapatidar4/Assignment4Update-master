b3af635ca29f9c2bb1a7066856e0f559436f46e3
Ankita Patidar <ankita4@usf.edu>
2020-04-26 02:06:46 -0400

    Add project files.

---
+++ IEXTrading/Models/Hospital.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace GHI.Models
+{
+    public class ChartRoot
+    {
+        public Hospital[] chart { get; set; }
+    }
+
+    public class Hospital
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public List<Hospital> Hospitals { get; set; }
+        public Guid Id { get; set; }
+        public string hospital_name { get; set; }
+        public string city { get; set; }
+        public string phone_number { get; set; }
+        public string hospital_overall_rating { get; set; }
+       
+    }
+}
